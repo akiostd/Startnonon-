f7c4e5f6bf06f9af900710d98fea87d00cf369b2
Akiostd <axlywb@gmail.com>
2025-04-27 07:37:22 +0700

    Create Log.ts

---
+++ Log.ts
@@ -0,0 +1,10 @@
+<!-- src/pages/login.ts -->
+<form action="/login" method="post" class="login-form">
+  <input
+    type="text"
+    name="handle"
+    placeholder="Enter your handle (...)"
+    required
+  />
+  <button type="submit">Log in</button>
+</form>
